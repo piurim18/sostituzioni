89c542ce571344938b78f2952a541b7b61323b33
meryame.jafour_isaro <meryame.jafour08@gmail.com>
2025-10-02 11:48:07 +0200

    jgvhjh

---
+++ src/FrameClassi.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FrameClassi extends JFrame{
+
+
+    JComboBox<String> classe = new JComboBox<>();
+    String [] classi = {"Disposizione","1^ A MAT", "1^ C IeFP", "1^ D INF", "1^ F MEC", "1^ G AGR", "2^ A MAT", "2^ C IeFP", "2^ D INF", "2^ F MEC", "2^ G AGR", "3^ A MAT", "3^ B TUR", "3^ C IeFP", "3^ D INF", "3^ F MEC", "3^ G AGR", "4^ A MAT", "4^ B TUR", "4^ D INF", "4^ F MEC", "5^ A MAT", "5^ B TUR", "5^ D INF", "5^ F MEC"};
+
+    public FrameClassi() {
+
+        setTitle("Seleziona Classe");
+
+        for (String d : classi) {
+            classe.addItem(d);
+        }
+
+        setLayout(new BorderLayout());
+
+        add(classe, BorderLayout.CENTER);
+
+        setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+
+        new FrameClassi();
+
+
+    }
+}
+
+
+
+
+
+
--- src/FrameDocenti.java
+++ src/FrameDocenti.java
@@ -4,7 +4,7 @@ import java.awt.*;
 public class FrameDocenti extends JFrame {
 
     JComboBox<String> docente = new JComboBox<>();
-    String [] dati = {"docente1","docente2","docente3"};
+    String [] dati = {"Cognome1", "Cognome2", "Cognome3", "Cognome4", "Cognome5", "Cognome6", "Cognome7", "Cognome8", "Cognome9", "Cognome10", "Cognome11", "Cognome12", "Cognome13", "Cognome14", "Cognome15", "Cognome16", "Cognome17", "Cognome18", "Cognome19", "Cognome20", "Cognome21", "Cognome22", "Cognome23", "Cognome24", "Cognome25", "Cognome26", "Cognome27", "Cognome28", "Cognome29", "Cognome30", "Cognome31", "Cognome32", "Cognome33", "Cognome34", "Cognome35", "Cognome36", "Cognome37", "Cognome38", "Cognome39", "Cognome40", "Cognome41", "Cognome42", "Cognome43", "Cognome44", "Cognome45", "Cognome46", "Cognome47", "Cognome48", "Cognome49", "Cognome50", "Cognome51", "Cognome52", "Cognome53", "Cognome54", "Cognome55", "Cognome56", "Cognome57", "Cognome58", "Cognome59", "Cognome60", "Cognome61", "Cognome62", "Cognome63", "Cognome64", "Cognome65", "Cognome66", "Cognome67", "Cognome68", "Cognome69", "Cognome70", "Cognome71", "Cognome72", "Cognome73"};
 
     public FrameDocenti() {
 
@@ -18,7 +18,7 @@ public class FrameDocenti extends JFrame {
 
         add(docente, BorderLayout.CENTER);
 
-        setSize(150, 50);
+        setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
