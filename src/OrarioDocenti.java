11a88f061f2a19c7c81854a32f4f485ec573090d
meryame.jafour_isaro <meryame.jafour08@gmail.com>
2025-10-02 10:49:09 +0200

    jgvhjh

--- src/OrarioDocenti.java
+++ src/OrarioDocenti.java
@@ -16,14 +16,14 @@ public class OrarioDocenti implements Serializable {
         docenti.add(docente);
     }
 
-    // Salva i dati in un file
+
     public void salvaOrario(String nomeFile) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nomeFile))) {
             oos.writeObject(this);
         }
     }
 
-    // Carica i dati da un file
+
     public static OrarioDocenti caricaOrario(String nomeFile) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nomeFile))) {
             return (OrarioDocenti) ois.readObject();
